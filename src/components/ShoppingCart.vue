01d62d125e34aca14a7f29827994893e2996f916
Jared Stevens <jared.b.stevens@gmail.com>
2018-12-03 12:19:28 -0600

    currency filter and shopping cart

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,7 +1,24 @@
-<template></template>
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ul>
+      <li v-for="product in products">{{product.title}} - {{ product.price | currency}} - {{product.quantity}}</li>
+    </ul>
+    <p>Total: {{ total | currency }}</p>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    products() {
+      return this.$store.getters.cartProducts;
+    },
+    total() {
+      return this.$store.getters.cartTotal;
+    }
+  }
+};
 </script>
 
 <style scoped>
